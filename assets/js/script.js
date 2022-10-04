19d9c998ea0698ebe493e66f62bf150b323fbdb0
paglaparatha <ritambalco@gmail.com>
2022-10-04 14:40:41 +0530

    added persisting functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
 const editableContainers = document.querySelectorAll(".editable")
 
 editableContainers.forEach(container => {
+
+    const id = container.getAttribute("id");
+    let savedContent = localStorage.getItem(`data__${id}`);
+
+    if (savedContent) {
+        container.innerHTML = savedContent;
+    }
+
     container.addEventListener("dblclick", e => {
-        container.setAttribute("contenteditable", true)
+        container.setAttribute("contenteditable", true);
         container.focus();
     });
 
     container.addEventListener("blur", e => {
-        container.setAttribute("contenteditable", false)
+        container.setAttribute("contenteditable", false);
+        const currentContent = container.innerHTML;
+        if (currentContent !== savedContent) {
+            persistData(id, currentContent);
+            savedContent = currentContent;
+            alert("Data successfully saved")
+        }
     })
-})
+});
+
+const persistData = (id, content) => {
+    localStorage.setItem(`data__${id}`, content);
+}
